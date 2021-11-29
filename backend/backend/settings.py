1d90bcb6d5ad21a18cdf0e58ac8ba1d33f577dc3
Yann Ville <yannv.consulting@gmail.com>
2021-11-29 15:09:09 +0100

    Ajout Filrage Personnalisé

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'investments.apps.InvestmentsConfig',
     'corsheaders',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
--- backend/investments/views.py
+++ backend/investments/views.py
@@ -1,11 +1,32 @@
+from django.db import models
+from django.db.models import fields, query
+import django_filters
+from django_filters.filters import CharFilter
 from rest_framework import serializers, viewsets
 from .models import Investments
 from .serializers import InvestmentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.filters import SearchFilter
+from .models import Investments
 
 import json
 
 # Create your views here.
 
+class InvestmentsFilter(django_filters.FilterSet):
+    ville = CharFilter(field_name="ville", lookup_expr='startswith')
+    etat_d_avancement = CharFilter(field_name="etat_d_avancement", lookup_expr='startswith')
+    class Meta:
+        model = Investments
+        fields = {
+            "ville",
+            "etat_d_avancement",
+        }
+
 class InvestementsViewSet(viewsets.ModelViewSet):
     queryset = Investments.objects.all()
     serializer_class = InvestmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_class = InvestmentsFilter
+
